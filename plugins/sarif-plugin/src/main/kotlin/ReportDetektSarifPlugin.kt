611f88cd39bb7c4185391a717a200215eec0cbfe
CherryPerry <yury.vlad@team.bumble.com>
2022-08-12 16:58:14 +0100

    111

--- plugins/sarif-plugin/src/main/kotlin/ReportDetektSarifPlugin.kt
+++ plugins/sarif-plugin/src/main/kotlin/ReportDetektSarifPlugin.kt
@@ -1,46 +1,35 @@
 import io.gitlab.arturbosch.detekt.Detekt
+import io.gitlab.arturbosch.detekt.extensions.DetektExtension
 import io.gitlab.arturbosch.detekt.report.ReportMergeTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
 
 class ReportDetektSarifPlugin : Plugin<Project> {
 
     override fun apply(target: Project) {
         target.plugins.withId("io.gitlab.arturbosch.detekt") {
+            target.extensions.configure<DetektExtension> {
+                baseline = target.file("detekt-baseline.xml")
+                config.from(target.rootProject.file("detekt.yml"))
+            }
+
+
             val detektTask = target.tasks.named("detekt", Detekt::class.java)
             detektTask.configure {
                 reports.sarif.required.set(true)
-                ignoreFailures = true
             }
 
             val rootProject = target.rootProject
-
-            /*rootProject.plugins.withId("appyx-collect-sarif") {
-                val mergeTask = rootProject.tasks.named(
-                    CollectLintSarifPlugin.MERGE_TASK_NAME,
-                    SarifMergeTask::class.java,
-                )
-
-                rootProject.tasks.named(
-                    CollectLintSarifPlugin.MERGE_TASK_NAME,
-                    SarifMergeTask::class.java,
-                ) {
-                    lintSarifFiles.from(
-                        detektTask.flatMap { it.sarifReportFile }
-                    )
-                    // for some reason automatic dependency does not works
-                    mustRunAfter(detektTask)
-                }
-            }*/
-
             rootProject.plugins.withId("appyx-collect-sarif") {
                 rootProject.tasks.named(
                     CollectSarifPlugin.MERGE_DETEKT_TASK_NAME,
                     ReportMergeTask::class.java,
                 ) {
                     input.from(
-                        detektTask.map { it.sarifReportFile }
+                        detektTask.map { it.sarifReportFile }.orNull
                     )
+                    mustRunAfter(detektTask)
                 }
             }
         }
